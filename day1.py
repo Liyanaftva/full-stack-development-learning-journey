96ffa361f94cb6f85f69e030ddd7996076d39d20
Liyanaftva <liyanaftva3245@gmail.com>
2026-06-09 11:56:52 +0530

    Day 1: Python basics - variables, data types, operators and type casting

---
+++ day1.py
@@ -0,0 +1,35 @@
+print("Hello World")
+print("Liyana")
+# print("Wayanad")
+print("Address:farook college")
+print('''liyana
+      calicut
+      wayanad''')
+'''print("""menha
+      calicut""")'''
+num=123
+print(num)
+print("The number is",num)
+print("the number is",num,"not a float number")
+print(f"The number {num} is not a float number")  #string formatting
+name="liyaa"
+num="123"
+print(type(name))
+print(type(num))
+a=10
+b=20
+print(a<b)
+print(type(a<b))
+print(a>b)
+print(type(a>b))
+num=123
+print(type(num))
+re_num=str(num)  #type casting
+print(type(re_num))
+num1=10
+num2=4
+result1=num1/num2  #division
+result2=num1//num2  #floor division
+result3=num1%num2  #remainder
+result4=num1**num2
+print(result1,result2,result3,result4)
